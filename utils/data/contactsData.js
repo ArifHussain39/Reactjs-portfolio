1bbbb99f204d4b336f46351795b898e0c46e30c6
Arif Hussain <arif3337179@gmail.com>
2024-03-30 22:11:51 +0500

    Update email

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,9 +1,9 @@
 export const contactsData = {
-    email: 'arif.hussain@exdigitals.net',
+    email: 'arifhussain@exdigitals.net',
     phone: '+923163401145',
     address: 'Sindh Society, Sukkur, Pakistan ',
     github: 'https://github.com/arifhussain39',
-    facebook: 'https://www.facebook.com/arif.hussain.ah/',
+    facebook: 'https://www.facebook.com/arif.chohan.ah/',
     linkedIn: 'https://www.linkedin.com/in/arif-hussain-chohan/',
     twitter: '',
     stackOverflow: '',
